8818fe86eed546c9c4b2267c0597defd07be93f7
Alihan Pehlivan <a.pehlivan35@gmail.com>
2013-04-21 20:59:21 +0300

    Revert "Revert "UPDATE""
    This reverts commit df543172ebc56f69cf2994ddbcf9d3b1d7af71f7.

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml"> 
 	<head>
-		<title><?php echo $lang['META_TITLE']; ?></title>
-		<meta name="description" content="<?php echo $lang['META_DESCRIPTION']; ?>">
-		<meta name="keywords" content="<?php echo $lang['META_KEYWORDS']; ?>">
-		<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $lang['META_CHARSET']; ?>">
+		<title><?=$lang['META_TITLE'];?></title>
+		<meta name="description" content="<?=$lang['META_DESCRIPTION'];?>">
+		<meta name="keywords" content="<?=$lang['META_KEYWORDS'];?>">
+		<meta http-equiv="Content-Type" content="text/html; charset=<?=$lang['META_CHARSET']; ?>">
 		
 		<link rel="stylesheet" href="css/style.css"/>
 		<link rel="stylesheet" href="css/buttons.css"/>
